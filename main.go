0addb21a58c511a5cfde43483573c6fb828b37f5
karsinogenic <muhmmadnaufl@gmail.com>
2022-03-12 11:08:13 +0700

    update

--- main.go
+++ main.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"fmt"
-
-	modulasi "github.com/karsinogenic/gomodul_tugas"
 )
 
+type item struct {
+	nama     string
+	stock    int
+	harga    int
+	nBelanja int
+	nTotal   int
+}
+
+// func coba(item [2]string) {
+// 	fmt.Println(item)
+// }
+
 func main() {
-	fmt.Println("coba")
-	bayar, pilihan := modulasi.Troli(100, 2)
-	if pilihan == true {
-		fmt.Println("belanja lagi")
-	}
-	fmt.Println("troli : ")
-	fmt.Println("kembalinya :", modulasi.Kasir(10000, bayar))
+	var minyak = item{"minyak", 10, 30000, 0, 0}
+	fmt.Println("Selamat datang mau belanja apa ? ")
+	fmt.Println("1.Minyak,stock:%s,harga:%s", minyak.stock, minyak.harga)
+	mbanyak, mtotal := minyak.Belanja1()
+	fmt.Println(mbanyak, mtotal)
+	//var terigu = item{"terigu", 15, 15000, 0}
+	//var item = [2]string{"minyak", "terigu"}
+
+	// coba(item)
+	// fmt.Println(minyak, terigu)
+
+	//banyak(item)
+
+	// switch belanja {
+	// case 1:
+	// 	bayar, pilihan := modulasi.Troli(100, 2)
+	// }
+	// fmt.Println("coba")
+	// bayar, pilihan := modulasi.Troli(100, 2)
+	// if pilihan == true {
+	// 	fmt.Println("belanja lagi")
+	// }
+	// fmt.Println("troli : ")
+	// fmt.Println("kembalinya :", modulasi.Kasir(10000, bayar))
 
 }
